41af56dea2ffbf836d2eabdcf9a4365b63beffee
JawaPrideAirdrop <162884769+JawaPrideAirdrop@users.noreply.github.com>
2024-09-06 00:38:50 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,18 @@
 {
+  "privateKey": "YOUR_PRIVATE_KEY",
   "networks": {
-    "yourNetworkKey": {
+    "mainnet": {
+      "name": "Ethereum Mainnet",
       "rpcUrl": "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
-      "privateKey": "YOUR_PRIVATE_KEY",
-      "tokenContractAddress": "0xYourTokenContractAddress"
+      "tokenContractAddress": "0xTokenContractAddress",
+      "explorer": "https://etherscan.io/"
+    },
+    "ropsten": {
+      "name": "Ropsten Testnet",
+      "rpcUrl": "https://ropsten.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      "tokenContractAddress": "0xTokenContractAddress",
+      "explorer": "https://ropsten.etherscan.io/"
     }
+    // Tambahkan jaringan lainnya jika perlu
   }
 }
