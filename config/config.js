d3100e5619800be0fa7da372b766a68bdf2b4a27
JawaPrideAirdrop <162884769+JawaPrideAirdrop@users.noreply.github.com>
2024-09-06 00:38:16 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,9 @@
-
+{
+  "networks": {
+    "yourNetworkKey": {
+      "rpcUrl": "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
+      "privateKey": "YOUR_PRIVATE_KEY",
+      "tokenContractAddress": "0xYourTokenContractAddress"
+    }
+  }
+}
